a2fea01f8b2c55b078a21ac2f32b7c87ae7f0c79
mxfng <maxhfung@gmail.com>
2025-05-23 19:16:16 -0700

    Rename to works and install lucide-static

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,6 +11,15 @@ const worksCollection = defineCollection({
   }),
 });
 
+const writingCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    date: z.string().transform((str) => new Date(str)),
+  }),
+});
+
 export const collections = {
   works: worksCollection,
+  writing: writingCollection,
 };
