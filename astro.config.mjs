a22399a5c3c5d31364e610762b8fbfe81f2a358d
mxfng <maxhfung@gmail.com>
2025-05-26 22:19:26 -0700

    Fix build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import path from "node:path";
-import mdx from "@astrojs/mdx";
-
-import react from "@astrojs/react";
 
 import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath } from "node:url";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react()],
-
   vite: {
     plugins: [tailwindcss()],
     resolve: {
