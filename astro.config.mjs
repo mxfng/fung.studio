3ecdc21098a4d45505de16fc04f1719bda980b39
mxfng <maxhfung@gmail.com>
2025-05-29 19:00:01 -0700

    Accent web font optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import path from "node:path";
-
-import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath } from "node:url";
+import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
