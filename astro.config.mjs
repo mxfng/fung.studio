97d89791c38c74d4a98c77d1b76cb816cfa3ee9e
mxfng <maxhfung@gmail.com>
2025-05-30 13:36:10 -0700

    Astro add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,18 @@ import { fileURLToPath } from "node:url";
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-		resolve: {
-			alias: {
-				"@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "./src"),
-			},
-		},
+  vite: {
+      plugins: [tailwindcss()],
+      resolve: {
+          alias: {
+              "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "./src"),
+          },
+      },
 	},
+
+  integrations: [react()],
 });
