5bd04082e740ed6c2856a55993d1a30325490dc3
Max Fung <maxhfung@gmail.com>
2025-06-04 03:13:49 -0700

    Better footer

--- src/content/social-links.ts
+++ src/content/social-links.ts
@@ -1,5 +1,6 @@
 interface SocialLink {
 	label: string;
+	short: string;
 	href: string;
 	description: string;
 }
@@ -7,21 +8,25 @@ interface SocialLink {
 const SOCIAL_LINKS: SocialLink[] = [
 	{
 		label: "Email",
+		short: "EM",
 		href: "mailto:max@fung.studio",
 		description: "max@fung.studio",
 	},
 	{
 		label: "LinkedIn",
+		short: "LI",
 		href: "https://www.linkedin.com/in/maxfung/",
 		description: "in/maxfung",
 	},
 	{
 		label: "Instagram",
+		short: "IG",
 		href: "https://www.instagram.com/fungkadelic/",
 		description: "@fungkadelic",
 	},
 	{
 		label: "GitHub",
+		short: "GH",
 		href: "https://www.github.com/mxfng",
 		description: "mxfng",
 	},
